1cd1cf04ffd5d45061e2223eff4fca05512d4678
Michal <mkbielawski@gmail.com>
2024-11-20 11:16:46 +0100

    service init

---
+++ src/main/kotlin/com/fourthwall/cinemaservice/CinemaServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.fourthwall.cinemaservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CinemaServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CinemaServiceApplication>(*args)
+}
