aaf5ee54ce8c7bd008a3f5b9eb6020bd3985d15f
Michal <mkbielawski@gmail.com>
2024-11-21 13:10:26 +0100

    - improve README.md and description

--- src/main/kotlin/com/fourthwall/cinemaservice/configuration/security/AuthenticationFacade.kt
+++ src/main/kotlin/com/fourthwall/cinemaservice/configuration/security/AuthenticationFacade.kt
@@ -4,7 +4,6 @@ import com.fourthwall.cinemaservice.domain.security.IAuthenticationFacade
 import org.springframework.stereotype.Component
 
 @Component
-@Suppress("UNCHECKED_CAST")
 class AuthenticationFacade : IAuthenticationFacade {
     override fun getUserEmail(): String {
         return "john.doe@mail.com"
